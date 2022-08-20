ed6bf2fe103a228a736cbd2105f4722620d76e82
raweber42 <ralf297@protonmail.com>
2022-08-20 18:31:05 +0200

    mlx init works

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,9 +6,70 @@
 /*   By: raweber <raweber@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 11:20:10 by raweber           #+#    #+#             */
-/*   Updated: 2022/08/20 11:20:31 by raweber          ###   ########.fr       */
+/*   Updated: 2022/08/20 17:45:04 by raweber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#if defined(__linux__)
+    # include "../minilibx_linux/mlx.h"
+#else
+    # include "../minilibx_macos/mlx.h"
+#endif
 # include <math.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
 # include <stdio.h>
+
+typedef struct s_cub {
+	// old fdf struct
+	// char	*argv;
+	// void	*mlx_ptr;
+	// void	*win_ptr;
+	// void	*mlx_img;
+	// int		win_width;
+	// int		win_height;
+	// float	zoom;
+	// float	height_zoom;
+	// int		default_color;
+	// float	angle;
+	// int		bits_per_pxl;
+	// int		line_len;
+	// int		endian;
+	// char	*mlx_img_addr;
+	int		x_offset;
+	int		y_offset;
+	int		height;
+	int		width;
+	int		**z_matrix;
+	int		**color_matrix;
+
+	int		iso_x;
+	int		iso_y;
+	int		iso_x1;
+	int		iso_y1;
+	struct s_mlx	*mlx_data;
+	// end old fdf struct
+}				t_cub;
+
+typedef struct s_mlx {
+	// old fdf struct
+	char	*argv;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*mlx_img;
+	int		win_width;
+	int		win_height;
+	float	zoom;
+	float	height_zoom;
+	int		default_color;
+	float	angle;
+	int		bits_per_pxl;
+	int		line_len;
+	int		endian;
+	char	*mlx_img_addr;
+}				t_mlx;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,54 @@
 /*   By: raweber <raweber@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 11:19:34 by raweber           #+#    #+#             */
-/*   Updated: 2022/08/20 11:19:51 by raweber          ###   ########.fr       */
+/*   Updated: 2022/08/20 18:30:45 by raweber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
+void	init_data(t_cub *data)
+{
+	data->mlx_data = (t_mlx *)ft_calloc(1, sizeof(t_mlx));
+	data->mlx_data->mlx_ptr = NULL;
+	data->mlx_data->mlx_img = NULL;
+	data->mlx_data->win_ptr = NULL;
+	data->mlx_data->mlx_img_addr = NULL;
+	data->mlx_data->win_width = 1000;
+	data->mlx_data->win_height = 1000;
+	data->mlx_data->angle = 0.9;
+	data->mlx_data->bits_per_pxl = 0;
+	data->mlx_data->line_len = 0;
+	data->mlx_data->endian = 0;
+}
+
+void	init_mlx(t_mlx *mlx_data)
+{
+	mlx_data->mlx_ptr = mlx_init();
+	mlx_data->win_ptr = mlx_new_window(mlx_data->mlx_ptr, \
+					mlx_data->win_width, mlx_data->win_height, "Cub3d");
+	mlx_data->mlx_img = mlx_new_image(mlx_data->mlx_ptr, \
+					mlx_data->win_width, mlx_data->win_height);
+	mlx_data->mlx_img_addr = mlx_get_data_addr(mlx_data->mlx_img, \
+					&mlx_data->bits_per_pxl, &mlx_data->line_len, &mlx_data->endian);
+}
+
 int	main(void)
 {
-	printf("HELLO\n");
+	t_cub	*data;
+	
+	data = (t_cub *)ft_calloc(1, sizeof(t_cub));
+	if (!data)
+		return (0);
+	// error checking goes here!
+	init_data(data);
+	// PARSING/READING GOES HERE
+	init_mlx(data->mlx_data);
+	// GAME LOOP GOES HERE
+
+	// HEEEEEERREEEEEEE DESTROYING ON SUNDAY
+	
+	// mlx_hook(data->mlx_data->win_ptr, 2, 0, &deal_key, data);
+	// mlx_hook(data->mlx_data->win_ptr, 17, 0, &destroy, data);
+	mlx_loop(data->mlx_data->mlx_ptr);
 }
